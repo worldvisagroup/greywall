cbde26723119078370ac9c9ee0a993aa0ebb9a5c
worldvisagroup <digital@worldvisa.in>
2024-10-23 18:09:54 +0530

    fix: added image

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,7 +3,14 @@ import { getDocuments } from "outstatic/server";
 export default async function Index() {
   const projects = await getData();
   return projects.map((project) => (
-    <h1 key={project.title}>{project.title}</h1>
+    <>
+      <h1 key={project.title}>{project.title}</h1>
+      {project?.imageOne ? (
+        <img src={project.imageOne} className="h-[200px] w-[200px]" />
+      ) : (
+        <></>
+      )}
+    </>
   ));
 }
 
