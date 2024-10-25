e61593a774e92993a6508a62720d3cf7d18dd553
hamid1882 <hussainhafeez1100@gmail.com>
2024-10-25 11:33:10 +0530

    fix: added cover image

--- app/components/Project.jsx
+++ app/components/Project.jsx
@@ -1,30 +1,9 @@
+import Image from "next/image";
 import Link from "next/link";
 import { getDocuments } from "outstatic/server";
 
 const Project = async () => {
   const data = await getAllProjects();
-  const projects = [
-    {
-      title: "Construction",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToxllQbjHl7FEd6AdyYjzL3BpdwnHm4wy4Tg&s",
-    },
-    {
-      title: "Civil ",
-      image:
-        "https://images.squarespace-cdn.com/content/v1/56f16317859fd038abca481a/1461097169552-87N4PKWL729EWYAV26BU/tumblr_mn8b8sLRb61rkz363o1_1280.jpg",
-    },
-    {
-      title: "Commercial Interior Design",
-      image:
-        "https://cdn.prod.website-files.com/65f5f834fb31bfc79e83b230/65fcc82f42342ec97c53ded1_latest_residential_design_trends.png",
-    },
-    {
-      title: "Residential Interior Design",
-      image:
-        "https://media.licdn.com/dms/image/D4D12AQFvjwGH7eRaig/article-cover_image-shrink_720_1280/0/1702011335822?e=2147483647&v=beta&t=-o8jWLRznBJm4HXkFBcjpon46oJg6GoiHxiGnEOpdpk",
-    },
-  ];
 
   return (
     <div id="project" className="bg-[#313131] text-white min-h-screen p-4 lg:p-8">
@@ -42,13 +21,7 @@ const Project = async () => {
                 <div className="small-desc font-semibold text-sm text-[#452c2c] group-hover:text-white/80 transition-all duration-500 relative z-10 font-montserrat">
                   Click to view projects
                 </div>
-                <div
-                  className="absolute inset-0 bg-cover bg-center z-0 opacity-50 group-hover:opacity-80 transition-opacity duration-500"
-                  style={{
-                    backgroundImage: `url(${projects[index % projects.length].image
-                      })`,
-                  }}
-                ></div>
+                <Image className="w-full h-full absolute inset-0 bg-cover bg-center z-0 opacity-50 group-hover:opacity-80 transition-opacity duration-500" src={collection.coverImage} alt={collection.title} width={400} height={400} />
                 <div className="go-corner absolute top-0 right-0 w-16 h-16 bg-gradient-to-br from-[#7C7C7C] to-[#272626] rounded-bl-[60px] flex items-center justify-center">
                   <div className="go-arrow text-white font-courier text-2xl -mt-2 -mr-2">
                     &rarr;
@@ -67,7 +40,7 @@ const Project = async () => {
 export default Project;
 
 export async function getAllProjects() {
-  const allServices = getDocuments("homepage-all-projects", ["title", "slug"]);
+  const allServices = getDocuments("homepage-all-projects", ["title", "slug", "coverImage"]);
 
   return allServices;
 }
