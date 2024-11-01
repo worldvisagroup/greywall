7178eeeabb7bd82ce529c6c7167e6386ad88f37e
worldvisagroup <digital@worldvisa.in>
2024-11-01 11:58:29 +0530

    font size change for mobile devices

--- app/components/Testimonials.jsx
+++ app/components/Testimonials.jsx
@@ -1,5 +1,4 @@
 import { load } from 'outstatic/server';
-import TestimonialCard from './TestimonialCard';
 import TestimonialSlider from "./TestimonialSlider";
 
 
@@ -10,7 +9,7 @@ const Testimonials = async () => {
   return (
     <div id="testimonials" className="bg-[#383838] py-8 lg:py-12 px-4 sm:px-6 lg:px-8">
       <div className="container mx-auto my-7">
-        <h1 className="text-[#FFEDE6] text-5xl mb-10 lg:text-6xl font-tenPearl lg:mb-[8%] text-center">
+        <h1 className="text-[#FFEDE6] text-4xl mb-10 lg:text-6xl font-tenPearl lg:mb-[8%] text-center">
           <span className="relative">
             Tes
             <span className="absolute bottom-0 left-0 w-full h-[2px] bg-white"></span>
