ca56868ec16df415801473bc0e7f59f1ee3c7865
vishnuvardhan2114 <vishnuvardhan14102002@gmail.com>
2025-02-04 18:54:16 +0530

    fix: Remove text color class from carousel slide button

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -126,7 +126,7 @@ const Slide = ({ slide, index, current, handleSlideClick }: SlideProps) => {
           )}
           <div className="flex justify-center">
             <button
-              className="mt-6 px-4 py-2 w-fit mx-auto sm:text-sm text-white h-12 border border-transparent text-xs flex justify-center items-center rounded-2xl hover:shadow-lg transition duration-200 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]"
+              className="mt-6 px-4 py-2 w-fit mx-auto sm:text-sm h-12 text-xs flex justify-center items-center rounded-2xl hover:shadow-lg transition duration-200 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]"
               onClick={handlePlayClick}
             >
               {button}
