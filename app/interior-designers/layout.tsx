d7ef6f1ec0075c2883cf9c74473d1e8ae7cc6d95
vishnuvardhan2114 <vishnuvardhan14102002@gmail.com>
2025-02-20 11:06:10 +0530

    feat(seo): Enhance metadata and add canonical link for interior designers page

--- app/interior-designers/layout.tsx
+++ app/interior-designers/layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
 import AnalyticsProvider from "../components/providers/AnalyticsProvider";
 import { Montserrat, Literata, Hurricane, Antic } from "next/font/google";
+import { Metadata } from "next";
 
 // const northWell = localFont({
 //   src: "./fonts/Northwell.ttf",
@@ -24,6 +25,18 @@ import { Montserrat, Literata, Hurricane, Antic } from "next/font/google";
 //   variable: "--font-ten-pearl",
 //   weight: "100 900",
 // });
+
+export const metadata: Metadata = {
+  title: "The Grey Wall - Best Residential & Commercial Interior Designer in Bangalore",
+  description: "Elevate your home, 2BHK, villa, office, or commercial space with The Grey Wall, the leading interior designer in Bangalore, specializing in bespoke, sustainable, and high-quality interiors.",
+  keywords: "Interior designer in Bangalore, 2bhk interior, Villa interiors designers, Commercial Interior Designer",
+
+  icons: {
+    icon: "/favicon2.ico",
+  },
+};
+
+
 const montserrat = Montserrat({ weight: ["400", "600"], subsets: ["latin"] });
 const literata = Literata({ weight: ["400", "700"], subsets: ["latin"] });
 const hurricane = Hurricane({ weight: ["400"], subsets: ["latin"] });
@@ -46,6 +59,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
+        <link rel="canonical" href="https://thegreywallinteriors.com/interior-designers" key="canonical" />
         {children}
       </body>
     </html>
